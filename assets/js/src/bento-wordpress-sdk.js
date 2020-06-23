8f00f7febf6d135095e3cf4c1428ef2a652ceb2e
Khaled Bentoumi <kbentoumi@gmail.com>
2020-06-23 09:39:08 +0200

    Pass parameter to bento wordpress sdk js

--- assets/js/src/bento-wordpress-sdk.js
+++ assets/js/src/bento-wordpress-sdk.js
@@ -2,6 +2,10 @@
   if (typeof bento$ != 'undefined') {
     bento$(function () {
       bento.view();
+      console.log(bento_wordpress_sdk_params);
+      if (bento_wordpress_sdk_params.woocommerce_enabled) {
+        console.log('test');
+      }
     });
   }
 })(jQuery);
--- inc/custom.php
+++ inc/custom.php
@@ -23,27 +23,41 @@ class Bento_Custom
     $bento_options = get_option('bento_settings');
     $bento_site_key = $bento_options['bento_site_key'];
 
-    if (!empty($bento_site_key)) {
-      $test = var_dump($bento_site_key);
-      wp_enqueue_script(
-        'bento-js',
-        "https://app.bentonow.com/{$bento_site_key}.js",
-        [],
-        false,
-        true
-      );
-
-      /*
-       * Enqueue scripts.
-       */
-      wp_enqueue_script(
-        'bento-wordpress-sdk-js',
-        plugins_url('assets/js/bento-wordpress-sdk.min.js', dirname(__FILE__)),
-        ['jquery', 'bento-js'],
-        $this->version,
-        true
-      );
+    if (empty($bento_site_key)) {
+      return;
     }
+
+    wp_enqueue_script(
+      'bento-js',
+      "https://app.bentonow.com/{$bento_site_key}.js",
+      [],
+      false,
+      true
+    );
+
+    /**
+     * Enqueue scripts.
+     */
+    wp_enqueue_script(
+      'bento-wordpress-sdk-js',
+      plugins_url('assets/js/bento-wordpress-sdk.min.js', dirname(__FILE__)),
+      ['jquery', 'bento-js'],
+      $this->version,
+      true
+    );
+
+    /**
+     * Pass parameters to Metorik JS.
+     */
+    $params = [
+      'woocommerce_enabled' => class_exists('WooCommerce'),
+    ];
+
+    wp_localize_script(
+      'bento-wordpress-sdk-js',
+      'bento_wordpress_sdk_params',
+      $params
+    );
   }
 }
 
