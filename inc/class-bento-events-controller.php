5ac2ed85feac1c940f19d0705f1c8bc18a5f4f8c
Estevão Costa <estevao90@gmail.com>
2022-04-27 23:37:43 -0300

    WP cron events and LD complete events

--- inc/class-bento-events-controller.php
+++ inc/class-bento-events-controller.php
@@ -12,8 +12,9 @@ if ( ! class_exists( 'Bento_Events_Controller', false ) ) {
 	 * Class Bento_Events_Controller
 	 */
 	class Bento_Events_Controller {
-		const BENTO_API_EVENT_ENDPOINT = 'https://app.bentonow.com/tracking/generic';
-		const EVENTS_QUEUE_OPTION_KEY  = 'bento_events_queue';
+		const BENTO_API_EVENT_ENDPOINT        = 'https://app.bentonow.com/tracking/generic';
+		const EVENTS_QUEUE_OPTION_KEY         = 'bento_events_queue';
+		const IS_SENDING_EVENTS_TRANSIENT_KEY = 'bento_sending_events';
 
 		/**
 		 * Bento configuration options
@@ -49,14 +50,28 @@ if ( ! class_exists( 'Bento_Events_Controller', false ) ) {
 				return;
 			}
 
-			$events_queue   = get_option( self::EVENTS_QUEUE_OPTION_KEY, array() );
+			if ( ! self::is_sending_events() ) {
+				$events_queue = get_option( self::EVENTS_QUEUE_OPTION_KEY, array() );
+			} else {
+				$events_queue = get_transient( self::EVENTS_QUEUE_OPTION_KEY );
+				if ( ! $events_queue ) {
+					$events_queue = array();
+				}
+			}
+
 			$events_queue[] = array(
 				'user_id' => $user_id,
 				'type'    => $type,
 				'email'   => $email,
 				'details' => $details,
 			);
-			update_option( self::EVENTS_QUEUE_OPTION_KEY, $events_queue );
+
+			if ( ! self::is_sending_events() ) {
+				update_option( self::EVENTS_QUEUE_OPTION_KEY, $events_queue );
+			} else {
+				set_transient( self::EVENTS_QUEUE_OPTION_KEY, $events_queue, DAY_IN_SECONDS );
+			}
+
 		}
 
 		/**
@@ -68,10 +83,8 @@ if ( ! class_exists( 'Bento_Events_Controller', false ) ) {
 		 * @param array  $details The event details.
 		 * @return bool True if event was sent successfully.
 		 */
-		private function send_event( $user_id, $type, $email, $details = array() ) {
-			$bento_options  = get_option( 'bento_settings' );
-			$bento_site_key = $bento_options['bento_site_key'];
-
+		private static function send_event( $user_id, $type, $email, $details = array() ) {
+			$bento_site_key = self::get_bento_option( 'bento_site_key' );
 			if ( empty( $bento_site_key ) ) {
 				return;
 			}
@@ -81,7 +94,7 @@ if ( ! class_exists( 'Bento_Events_Controller', false ) ) {
 				'type'    => $type,
 				'email'   => $email,
 				'details' => $details,
-				'fields'  => $this->get_user_fields( $user_id ),
+				'fields'  => self::get_user_fields( $user_id ),
 			);
 
 			$response      = wp_remote_post(
@@ -104,7 +117,7 @@ if ( ! class_exists( 'Bento_Events_Controller', false ) ) {
 		 * @param int $user_id The user ID.
 		 * @return array The user fields.
 		 */
-		private function get_user_fields( $user_id ) {
+		private static function get_user_fields( $user_id ) {
 			if ( empty( $user_id ) ) {
 				return array();
 			}
@@ -134,6 +147,82 @@ if ( ! class_exists( 'Bento_Events_Controller', false ) ) {
 		 */
 		public static function init() {
 			add_action( 'init', array( __CLASS__, 'load_events_controllers' ) );
+
+			// Add a cron job to send events to Bento.
+			add_filter( 'cron_schedules', array( __CLASS__, 'add_events_cron_interval' ) ); // phpcs:ignore WordPress.WP.CronInterval
+			add_action( 'bento_send_events_hook', array( __CLASS__, 'bento_send_events_hook' ) );
+			if ( ! wp_next_scheduled( 'bento_send_events_hook' ) ) {
+				wp_schedule_event( time(), 'bento_send_events_interval', 'bento_send_events_hook' );
+			}
+		}
+
+		/**
+		 * Unschedule the Bento events cron job. Used when the plugin is deactivated.
+		 */
+		public static function unschedule_bento_events_cron() {
+			$timestamp = wp_next_scheduled( 'bento_send_events_hook' );
+			wp_unschedule_event( $timestamp, 'bento_send_events_hook' );
+		}
+
+		/**
+		 * Check if bento is sending events.
+		 */
+		private static function is_sending_events() {
+			return true === get_transient( self::IS_SENDING_EVENTS_TRANSIENT_KEY );
+		}
+
+		/**
+		 * Send events in the queue to Bento.
+		 */
+		public static function bento_send_events_hook() {
+			if ( self::is_sending_events() ) {
+				WP_DEBUG && error_log( 'Bento is already sending events.' ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+				return; // Already sending events.
+			}
+			WP_DEBUG && error_log( 'Sending Bento events.' ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+
+			// set the transient to true so we know we're sending events.
+			set_transient( self::IS_SENDING_EVENTS_TRANSIENT_KEY, true, HOUR_IN_SECONDS * 6 );
+
+			$events_queue     = get_option( self::EVENTS_QUEUE_OPTION_KEY, array() );
+			$new_events_queue = array();
+			foreach ( $events_queue as $event ) {
+				$event_status = self::send_event( $event['user_id'], $event['type'], $event['email'], $event['details'] );
+				WP_DEBUG && error_log( 'Bento event "' . $event['type'] . '" sended. Status: ' . $event_status ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+				if ( ! $event_status ) {
+					// event was not sent successfully.
+					$new_events_queue[] = $event;
+				}
+			}
+
+			// merge temporary queue with the permanent queue.
+			$temporary_queue = get_transient( self::EVENTS_QUEUE_OPTION_KEY );
+			if ( ! empty( $temporary_queue ) && is_array( $temporary_queue ) ) {
+				$new_events_queue = array_merge( $new_events_queue, $temporary_queue );
+			}
+			delete_transient( self::EVENTS_QUEUE_OPTION_KEY );
+			update_option( self::EVENTS_QUEUE_OPTION_KEY, $new_events_queue );
+
+			// finish sending events.
+			delete_transient( self::IS_SENDING_EVENTS_TRANSIENT_KEY );
+			WP_DEBUG && error_log( 'Bento events sended.' ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+		}
+
+		/**
+		 * Add bento cron interval.
+		 *
+		 * @param array $schedule The schedule.
+		 * @return array
+		 */
+		public static function add_events_cron_interval( $schedule ) {
+			$interval = self::get_bento_option( 'bento_events_recurrence' );
+			if ( ! empty( $interval ) ) {
+				$schedule['bento_send_events_interval'] = array(
+					'interval' => MINUTE_IN_SECONDS * $interval,
+					'display'  => __( 'Bento Send Events Interval', 'bentonow' ),
+				);
+			}
+			return $schedule;
 		}
 
 		/**
--- inc/events-controllers/class-learndash-bento-events.php
+++ inc/events-controllers/class-learndash-bento-events.php
@@ -18,25 +18,90 @@ if ( defined( 'LEARNDASH_VERSION' ) && ! class_exists( 'LearnDash_Bento_Events',
 		 *  @return void
 		 */
 		public function __construct() {
-			add_action( 'learndash_course_completed', array( $this, 'learndash_course_completed_event' ) );
-		}
-
-		/**
-		 * Send a LearnDash course completed event.
-		 *
-		 * @param array $course_data The course data.
-		 * @return void
-		 */
-		public function learndash_course_completed_event( $course_data ) {
-			$this->enqueue_event(
-				$course_data['user']->ID,
+			add_action(
 				'learndash_course_completed',
-				$course_data['user']->user_email,
-				array(
-					'course_id'        => $course_data['course']->ID,
-					'course_name'      => $course_data['course']->post_title,
-					'course_completed' => $course_data['course_completed'],
-				)
+				function( $ld_data ) {
+					$this->enqueue_event(
+						$ld_data['user']->ID,
+						'learndash_course_completed',
+						$ld_data['user']->user_email,
+						array(
+							'course_id'        => $ld_data['course']->ID,
+							'course_name'      => $ld_data['course']->post_title,
+							'course_completed' => $ld_data['course_completed'],
+						)
+					);
+				}
+			);
+
+			add_action(
+				'learndash_lesson_completed',
+				function( $ld_data ) {
+					$this->enqueue_event(
+						$ld_data['user']->ID,
+						'learndash_lesson_completed',
+						$ld_data['user']->user_email,
+						array(
+							'lesson_id'       => $ld_data['lesson']->ID,
+							'lesson_name'     => $ld_data['lesson']->post_title,
+							'course_id'       => $ld_data['course']->ID,
+							'course_name'     => $ld_data['course']->post_title,
+							'course_progress' => $ld_data['progress'],
+						)
+					);
+				}
+			);
+
+			add_action(
+				'learndash_topic_completed',
+				function( $ld_data ) {
+					$this->enqueue_event(
+						$ld_data['user']->ID,
+						'learndash_topic_completed',
+						$ld_data['user']->user_email,
+						array(
+							'topic_id'        => $ld_data['topic']->ID,
+							'topic_name'      => $ld_data['topic']->post_title,
+							'lesson_id'       => $ld_data['lesson']->ID,
+							'lesson_name'     => $ld_data['lesson']->post_title,
+							'course_id'       => $ld_data['course']->ID,
+							'course_name'     => $ld_data['course']->post_title,
+							'course_progress' => $ld_data['progress'],
+						)
+					);
+				}
+			);
+
+			add_action(
+				'learndash_quiz_completed',
+				function( $quizdata, $wp_user ) {
+					$this->enqueue_event(
+						$wp_user->ID,
+						'learndash_quiz_completed',
+						$wp_user->user_email,
+						array(
+							'quiz_id'             => $quizdata['quiz'],
+							'quiz_name'           => get_the_title( $quizdata['quiz'] ),
+							'score'               => $quizdata['score'],
+							'count'               => $quizdata['count'],
+							'question_show_count' => $quizdata['question_show_count'],
+							'pass'                => $quizdata['pass'],
+							'points'              => $quizdata['points'],
+							'total_points'        => $quizdata['total_points'],
+							'percentage'          => $quizdata['percentage'],
+							'timespent'           => $quizdata['timespent'],
+							'has_graded'          => $quizdata['has_graded'],
+							'course_id'           => ! empty( $quizdata['course'] ) ? $quizdata['course']->ID : 0,
+							'course_name'         => ! empty( $quizdata['course'] ) ? $quizdata['course']->post_title : '',
+							'lesson_id'           => ! empty( $quizdata['lesson'] ) ? $quizdata['lesson']->ID : 0,
+							'lesson_name'         => ! empty( $quizdata['lesson'] ) ? $quizdata['lesson']->post_title : '',
+							'topic_id'            => ! empty( $quizdata['topic'] ) ? $quizdata['topic']->ID : 0,
+							'topic_name'          => ! empty( $quizdata['topic'] ) ? $quizdata['topic']->post_title : '',
+						)
+					);
+				},
+				10,
+				2
 			);
 		}
 	}
