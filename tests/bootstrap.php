c0eb6bf88790e9f1d6e0276ff0afb303b5ba4a86
ziptied <zachary.oakes@gmail.com>
2025-09-22 12:35:28 +0900

    path adjustment for gh test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
-define('ABSPATH', __DIR__ . '/../');
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/../');
+}
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Define required WordPress constants
